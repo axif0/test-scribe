5d96806c1e4857457647e7ee575c3123d34e9821
axif <muhamadasif570@gmail.com>
2025-01-18 01:58:43 +0600

    modifify fix

--- src/scribe_data/check/check_missing_forms/check_missing_forms.py
+++ src/scribe_data/check/check_missing_forms/check_missing_forms.py
@@ -8,20 +8,33 @@ from scribe_data.utils import (
     language_metadata,
     data_type_metadata,
 ) 
-from pr_body import pr_body
+
 from pathlib import Path
 iso_to_qid = {
     lang_data["iso"]: lang_data["qid"]
     for lang, lang_data in language_metadata.items()
     if "iso" in lang_data and "qid" in lang_data
 }
-
+ 
 # Add sub-languages to iso_to_qid
 for lang, lang_data in language_metadata.items():
     if "sub_languages" in lang_data:
         for sub_lang, sub_lang_data in lang_data["sub_languages"].items():
             if "iso" in sub_lang_data and "qid" in sub_lang_data:
                 iso_to_qid[sub_lang_data["iso"]] = sub_lang_data["qid"]
+
+for lang, lang_data in language_metadata.items():
+    if "iso" in lang_data and "qid" in lang_data:
+        iso_to_qid[lang_data["iso"]] = lang_data["qid"]
+
+print(iso_to_qid)
+
+languages = []
+for lang, lang_data in language_metadata.items():
+    if "sub_languages" in lang_data:
+        languages.extend(lang_data["sub_languages"].keys())
+    else:
+        languages.append(lang)
  
  
 def get_missing_features(result_sparql, result_dump):
@@ -60,15 +73,11 @@ if __name__ == "__main__":
         sys.exit(1)
 
     dump_path = Path(sys.argv[1])
-    # query_dir = Path(sys.argv[2])
-
-    # Read the query file using the provided path
     result_sparql = parse_sparql_files()
-    # print(result_sparql)
-    
+
     print("Extracting Wiki lexeme dump")
     result_dump = extract_dump_forms(
-        languages=list(language_metadata.keys()),
+        languages=languages,
         data_types=list(data_type_metadata.keys()),
         file_path=dump_path
     )
