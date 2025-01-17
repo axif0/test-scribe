8bd9cf5dea6b9aea1d057bef3eb05fe5344c15d2
axif <muhamadasif570@gmail.com>
2025-01-18 02:07:23 +0600

    fix

--- src/scribe_data/check/check_missing_forms/check_missing_forms.py
+++ src/scribe_data/check/check_missing_forms/check_missing_forms.py
@@ -78,7 +78,6 @@ def main():
     print("Extracting Wiki lexeme dump...")
     result_dump = extract_dump_forms(
         languages=languages,
-        languages=languages,
         data_types=list(data_type_metadata.keys()),
         file_path=dump_path
     )
